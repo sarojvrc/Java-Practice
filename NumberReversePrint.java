cb31cd672c038b12730dab89bb5dbd8b105a12fa
sarojvrc <saroj.vrc@gmail.com>
2021-06-26 01:56:20 +0530

    practice 25 June

---
+++ NumberReversePrint.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class NumberReversePrint {
+	public static void main(String args[]) {
+		int count = 10;
+		for(int i=1; i<=4; i++) {
+			for(int j=10; j<=1; j--) {
+				count--;
+				System.out.print(count + " ");
+			}
+			System.out.println();
+		}
+	}
+}

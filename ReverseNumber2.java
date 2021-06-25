bcc70787968e9ac25c517f23d2d4678aa40fcf09
sarojvrc <saroj.vrc@gmail.com>
2021-06-26 03:12:28 +0530

    25 June Practice

--- ReverseNumber2.java
+++ ReverseNumber2.java
@@ -1,12 +1,12 @@
 package practice;
 
-public class NumberReversePrint {
+public class ReverseNumber2 {
 	public static void main(String args[]) {
 		int count = 11;
 		for(int i=1; i<=4; i++) {
-			for(int j=4; j>=i; j--) {
+			for(int j=1; j<=i; j++) {
 				count--;
-				System.out.print(count + " ");
+				System.out.print(count+" ");
 			}
 			System.out.println();
 		}

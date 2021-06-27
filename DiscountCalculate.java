6b5625dae73a6b30579b86602517399311ce0ab3
sarojvrc <saroj.vrc@gmail.com>
2021-06-27 14:59:33 +0530

    27 june practice

---
+++ DiscountCalculate.java
@@ -0,0 +1,23 @@
+package practice;
+import java.util.Scanner;
+
+public class DiscountCalculate {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the MRP of the Saree: ");
+		int sareeMRP = sc.nextInt();
+		System.out.println("Enter the selling price of Saree: ");
+		int sareePrice = sc.nextInt();
+		System.out.println("Enter the no of Saree you want to buy: ");
+		int sareeNo = sc.nextInt();
+		
+		int totalSareeMRP = sareeMRP * sareeNo;
+		int totalSellingPrice = sareePrice * sareeNo;
+		int discount = ((totalSareeMRP-totalSellingPrice)/totalSareeMRP)*100;
+		
+		System.out.println("You bought total " + sareeNo +" no of Saree(s)");
+		System.out.println("Your total amount is " + totalSellingPrice);
+		System.out.println("You got a discount of " + discount +" %");
+		
+	}
+}

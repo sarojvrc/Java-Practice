1af9ee87a26c10b7d64bdcb994e7a46457429b5d
sarojvrc <saroj.vrc@gmail.com>
2021-06-30 02:13:04 +0530

    30 june

---
+++ SwitchCaseExample.java
@@ -0,0 +1,51 @@
+package practice;
+
+import java.util.Scanner;
+
+public class SwitchCaseExample {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a no from 1 to 12: ");
+		int no = sc.nextInt();
+		switch(no) {
+		case 1:
+			System.out.println("January");
+			break;
+		case 2:
+			System.out.println("February");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		case 6:
+			System.out.println("June");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("Auguest");
+			break;
+		case 9:
+			System.out.println("September");
+			break;
+		case 10:
+			System.out.println("October");
+			break;
+		case 11:
+			System.out.println("November");
+			break;
+		case 12:
+			System.out.println("December");
+			break;
+		default:
+			System.out.println("Print enter a no between 1 to 12.");
+		}
+	}
+}

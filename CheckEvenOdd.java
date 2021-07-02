d39f8c6b1220a011eb69346bd7550b7d3d7147bc
sarojvrc <saroj.vrc@gmail.com>
2021-07-02 11:37:47 +0530

    Jule 2

---
+++ CheckEvenOdd.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class CheckEvenOdd {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int no = sc.nextInt();
+		if(no%2 == 0) {
+			System.out.println(no+" is a even number.");
+		}else {
+			System.out.println(no+" is a odd number.");
+		}
+	}
+}

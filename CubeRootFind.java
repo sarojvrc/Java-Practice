ee2ea98abd03196b6ed104d3d94558987d9d2282
sarojvrc <saroj.vrc@gmail.com>
2021-07-02 11:25:00 +0530

    July 3

---
+++ CubeRootFind.java
@@ -0,0 +1,15 @@
+package practice;
+
+import java.util.Scanner;
+
+public class CubeRootFind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		long number = sc.nextLong();
+		System.out.println("The cube of the "+number+" is "+number*number*number);
+	}
+
+}

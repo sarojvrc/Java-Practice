b7571b50fa61663f30879a7e829c216ccc58ed82
sarojvrc <saroj.vrc@gmail.com>
2021-07-10 01:10:24 +0530

    July 10

---
+++ AssciValue.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class AssciValue {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a char: ");
+		char chr = sc.next().charAt(0);
+		int aski = chr;
+		System.out.println("The "+chr+" ascii value is "+aski);
+		
+	}
+
+}
---
+++ RandomNumberWithin1234.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class RandomNumberWithin1234 {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		for(int i=1; i<=4; i++) {
+			for(int j=1; j<=4; j++) {
+				for(int k=1; k<=4; k++) {
+					if(k!=i && k!=j && i!=j) {
+						count++;
+						System.out.println(i+""+j+""+k);
+					}
+					
+				}
+			}
+		}
+		System.out.println("The count is "+count);
+	}
+
+}

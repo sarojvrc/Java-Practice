68a28d049257f1ee61c27b1800d4e2bfecbce900
sarojvrc <saroj.vrc@gmail.com>
2021-07-11 23:27:55 +0530

    July 11

--- AssciValue.java
+++ AssciValue.java
@@ -10,7 +10,8 @@ public class AssciValue {
 		System.out.print("Enter a char: ");
 		char chr = sc.next().charAt(0);
 		int aski = chr;
-		System.out.println("The "+chr+" ascii value is "+aski);
+		//System.out.println("The "+chr+" ascii value is "+aski);
+		System.out.println("The ASCII value of "+chr+" is "+aski);
 		
 	}
 

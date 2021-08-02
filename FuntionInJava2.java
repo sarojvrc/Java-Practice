ae3cf2d54aa8178a33f77f2a0be37d073d5c4e15
Saroj Kumar <53892172+sarojvrc@users.noreply.github.com>
2021-08-02 12:23:27 +0530

    Add files via upload

---
+++ FuntionInJava2.java
@@ -0,0 +1,15 @@
+package dsa;
+
+public class FuntionInJava2 {
+	
+	static int mult(int a, int b) {
+		return a * b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mult is "+mult(15,45));
+
+	}
+
+}
